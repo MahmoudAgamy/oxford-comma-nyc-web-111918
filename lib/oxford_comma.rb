53e5254034e13095a89461be95e719af4f220c1e
Mahmoud Agamy <agamyv@gmail.com>
2018-11-16 21:59:30 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,9 @@ def oxford_comma(array)
   if array.length == 1 
     return array[0]
   elsif array.length == 2
-    return ""
+    return "#{array[0]} and #{array[1]}"
   elsif array.length == 3
+    return "#{array[0]} and #{array[1]}"
   end
 
 end
